408980e9b566b9cc2f880970e699dea1366704b7
girishkamat <girishkamat@users.noreply.github.com>
2022-07-01 14:30:48 +0100

    MRC-48 Fixed logging of config error on startup

--- app/config/AppConfig.scala
+++ app/config/AppConfig.scala
@@ -17,6 +17,7 @@
 package config
 
 import calculator.CalculatorConfig
+import org.slf4j.{ Logger, LoggerFactory }
 import play.api.Configuration
 
 import javax.inject.{ Inject, Singleton }
@@ -24,14 +25,19 @@ import javax.inject.{ Inject, Singleton }
 @Singleton
 class AppConfig @Inject() (config: Configuration) {
 
+  private val logger: Logger = LoggerFactory.getLogger(getClass)
+
   val appName: String = config.get[String]("appName")
 
   val calculatorConfig: CalculatorConfig =
     CalculatorConfigParser
       .parse(config)
       .fold(
-        invalidConfigError =>
-          throw new IllegalArgumentException(invalidConfigError.map(_.message).toList.mkString(", ")),
+        invalidConfigError => {
+          val errors = invalidConfigError.map(_.message).toList.mkString(", ")
+          logger.error(s"Failed to parse calculator-config. Errors are '$errors'")
+          throw new RuntimeException("Failed to parse calculator-config")
+        },
         fyConfigs => CalculatorConfig(fyConfigs)
       )
 }
